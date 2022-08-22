33baa7ec68049dcf15056b96474acc2fe5d802c9
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-08-22 00:22:10 -0300

    feat: adiciona ssr para stitches

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import React from "react";
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 
-import { getCssText } from '../../stitches.config';
+import { getCssText } from "../../stitches.config";
 
 export default class Document extends NextDocument {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+	render() {
+		return (
+			<Html lang="en">
+				<Head>
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link
+						rel="preconnect"
+						href="https://fonts.gstatic.com"
+						crossOrigin="true"
+					/>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400&family=Noto+Sans:wght@400;500;600;700;800&display=swap"
+						rel="stylesheet"
+					/>
+
+					<style
+						id="stitches"
+						dangerouslySetInnerHTML={{ __html: getCssText() }}
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
