3bf212390432a63299e6431a411e5083e20b9591
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-08-28 23:31:54 -0300

    feat: adiciona conteúdo do footer

--- src/components/Infra/Footer.tsx
+++ src/components/Infra/Footer.tsx
@@ -1,1 +1,19 @@
-export const Footer = () => <footer></footer>;
+import { styled } from "../../../stitches.config";
+
+const Container = styled("footer", {
+	flexCenter: "row",
+	padding: "20px 0px",
+});
+
+const Copyright = styled("small", {
+	fontWeight: 900,
+	fontFamily: "$font__util",
+	color: "$font",
+	opacity: 0.4,
+});
+
+export const Footer = () => (
+	<Container>
+		<Copyright>Gabriel Lima - {new Date().getFullYear()}</Copyright>
+	</Container>
+);
