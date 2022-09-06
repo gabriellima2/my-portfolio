57d0440cf21bedbad3f7d8fd4478f40847a7fd94
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-09-06 18:29:08 -0300

    feat: adiciona favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default class Document extends NextDocument {
 		return (
 			<Html lang="en">
 				<Head>
+					<link rel="icon" href="/favicon.svg" />
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link
 						rel="preconnect"
