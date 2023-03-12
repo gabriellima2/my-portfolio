12079f27bc0eae10d92baf524368019b94ceec53
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-03-12 19:12:37 -0300

    fix: melhora visualização do contéudo do menu de navegação

--- src/components/MenuNavigation/MenuNavigation.tsx
+++ src/components/MenuNavigation/MenuNavigation.tsx
@@ -13,7 +13,7 @@ export const MenuNavigation = () => {
 	return (
 		<>
 			{isOpen && (
-				<nav className="flex items-center justify-center absolute left-0 top-0 z-40 transition-colors w-screen h-screen bg-main/90 dark:bg-mainDark/90">
+				<nav className="flex items-center justify-center absolute left-0 top-0 z-40 transition-colors w-screen h-screen bg-main/90 dark:bg-mainDark/90 backdrop-blur">
 					<Navigation />
 				</nav>
 			)}
