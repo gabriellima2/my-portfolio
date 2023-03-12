253bba43173a596f7b031483746f829346361364
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-03-12 19:10:56 -0300

    fix: melhora responsividade dos projetos

--- src/components/MyProjects.tsx
+++ src/components/MyProjects.tsx
@@ -21,14 +21,14 @@ const Project = ({ image, ...props }: ProjectInfo) => (
 				alt={`Demonstração do projeto ${props.name}`}
 				layer
 				className="rounded-sm"
-				containerStyle="w-[90vw] h-[66vw] md:w-[300px] md:h-[220px] lg:w-[260px] lg:h-[190px] xl:w-[300px] xl:h-[220px]"
+				containerStyle="w-[90vw] h-[68vw] md:w-[300px] md:h-[220px] lg:w-[260px] lg:h-[190px] xl:w-[300px] xl:h-[220px]"
 			/>
 		</a>
 	</li>
 );
 
 export const MyProjects = ({ projects }: MyProjectsProps) => (
-	<ul className="w-full md:max-h-[55vh] md:pr-2 md:overflow-y-auto md:overflow-x-hidden grid grid-cols-1 md:grid-cols-2 gap-4 place-items-center">
+	<ul className="w-full lg:min-w-[520px] xl:min-w-0 md:max-h-[55vh] md:pr-2 md:overflow-y-auto md:overflow-x-hidden grid grid-cols-1 md:grid-cols-2 gap-4 place-items-center">
 		{projects.map((project) => (
 			<Project {...project} key={project.id} />
 		))}
