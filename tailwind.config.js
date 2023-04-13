05009e43224db997246f5c2192781ef25758ea57
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-04-13 16:23:08 -0300

    chore: remove old styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,23 +6,8 @@ module.exports = {
 	darkMode: "class",
   theme: {
     extend: {
-			colors: {
-				mainDark: "#0A0A0B",
-				utilDark: "#D0D0D0",
-				fontDark: "#F0F0F0",
-				contrastDark: "#0A0A0A",
-				accentDark: "#F0F0F0",
-
-				main: "#F0F0F0",
-				util: "#111212",
-				font: "#0A0A0B",
-				contrast: "#F5F3F3",
-				accent: "#0A0A0B",
-			},
-			fontFamily: {
-				main: ["Lato", "sans-serif"],
-				accent: ["Lora", "serif"],
-			},
+			colors: {},
+			fontFamily: {},
 		},
   },
   plugins: [],
