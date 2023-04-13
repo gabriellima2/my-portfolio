7a8c03a30ca6fb19720547ffa3b1213af51a855e
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-04-13 16:43:26 -0300

    feat: add new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,21 @@ module.exports = {
 	darkMode: "class",
   theme: {
     extend: {
-			colors: {},
+			colors: {
+				backgroundDark: "#1A1A1A",
+				mainDark: "#F8F8F8",
+				headingTextDark: "#F8F8F8",
+				defaultTextDark: "#8C8C8C",
+				utilPrimaryDark: "#1F1F1F",
+				utilSecondaryDark: "#8C8C8C",
+
+				background: "#F8F8F8",
+				main: "#202020",
+				headingText: "#202020",
+				defaultText: "#5A5A5A",
+				utilPrimary: "#EFEFEF",
+				utilSecondary: "#DDDDDD",
+			},
 			fontFamily: {
 				default: "'Anuphan', sans-serif"
 			},
