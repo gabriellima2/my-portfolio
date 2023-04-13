a780f387acd46dd6083ab950f2ffb2fcc8358354
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-04-13 18:44:46 -0300

    style: improves file organization of the theme-context

--- src/contexts/theme-context/theme-provider.test.tsx
+++ src/contexts/theme-context/theme-provider.test.tsx
@@ -1,57 +1,10 @@
-import { TWithChildren } from "@/@types/TWithChildren";
-import { ThemeContext } from "./theme-context";
-import { Themes } from "./@types/Themes";
 import { render, screen } from "@testing-library/react";
-import { ButtonWithSpecificTheme } from "./mocks/button-with-specific-theme";
-import { useEffect, useState } from "react";
 import { vi } from "vitest";
+
+import { ThemeProvider } from ".";
+
+import { ButtonWithSpecificTheme } from "./mocks/button-with-specific-theme";
 import { simulateClick } from "@/__mocks__/simulate-click";
-
-type ThemeProviderProps = TWithChildren & {
-	initialTheme: Themes;
-	documentClassName: string;
-	persistor?: (theme: Themes) => Promise<void> | void;
-};
-
-export const ThemeProvider = (props: ThemeProviderProps) => {
-	const { initialTheme, documentClassName, persistor, children } = props;
-	const [currentTheme, setCurrentTheme] = useState(initialTheme);
-
-	const handlePersistUserPreference = () => {
-		if (!persistor) return;
-		persistor(currentTheme);
-	};
-
-	const handleDocumentClassNameChange = () => {
-		const html = document.documentElement;
-		if (currentTheme === "dark" && !html.classList.contains(documentClassName))
-			return html.classList.add(documentClassName);
-
-		if (currentTheme === "light" && html.classList.contains(documentClassName))
-			return html.classList.remove(documentClassName);
-	};
-
-	const handleToggleTheme = () => {
-		if (currentTheme === "dark") return setCurrentTheme("light");
-		setCurrentTheme("dark");
-	};
-
-	useEffect(() => {
-		handleDocumentClassNameChange();
-		handlePersistUserPreference();
-	}, [currentTheme]);
-
-	return (
-		<ThemeContext.Provider
-			value={{
-				currentTheme,
-				handleToggleTheme,
-			}}
-		>
-			{children}
-		</ThemeContext.Provider>
-	);
-};
 
 const PERSISTOR = vi.fn();
 const DOCUMENT_CLASSNAME = "any_class";
