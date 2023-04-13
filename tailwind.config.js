d3bfe7560be0667c6f020a635e1c057653295bda
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-04-13 17:06:50 -0300

    style: improve color names

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,16 @@ module.exports = {
   theme: {
     extend: {
 			colors: {
-				backgroundDark: "#1A1A1A",
-				mainDark: "#F8F8F8",
-				headingTextDark: "#F8F8F8",
+				mainDark: "#1A1A1A",
+				brandDark: "#F8F8F8",
+				headingDark: "#F8F8F8",
 				defaultTextDark: "#8C8C8C",
 				utilPrimaryDark: "#1F1F1F",
 				utilSecondaryDark: "#8C8C8C",
 
-				background: "#F8F8F8",
-				main: "#202020",
-				headingText: "#202020",
+				main: "#F8F8F8",
+				brand: "#202020",
+				heading: "#202020",
 				defaultText: "#5A5A5A",
 				utilPrimary: "#EFEFEF",
 				utilSecondary: "#DDDDDD",
