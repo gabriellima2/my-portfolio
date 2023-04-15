04fc2ce117cf1f1562e7552a0d8cc02cabb6872b
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-04-15 15:59:52 -0300

    feat: add responsive spacings

--- src/components/common/MaxWidthContainer.tsx
+++ src/components/common/MaxWidthContainer.tsx
@@ -5,7 +5,7 @@ type MaxWidthContainerProps = TWithChildren;
 export const MaxWidthContainer = (props: MaxWidthContainerProps) => {
 	const { children } = props;
 	return (
-		<div className="center--col p-21">
+		<div className="center--col p-6 sm:p-11 md:p-16 lg:p-21">
 			<div className="w-full max-w-[1440px]">{children}</div>
 		</div>
 	);
