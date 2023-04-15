6c323fca5a950c57db12b541df7f146d9ccf247c
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-04-15 18:54:40 -0300

    feat: adds the default anchor component to the different route

--- src/components/common/Anchors/components/Anchor.test.tsx
+++ src/components/common/Anchors/components/Anchor.test.tsx
@@ -40,6 +40,7 @@ const renderComponent = (props: IAnchor = DEFAULT_ANCHOR) =>
 	render(<Anchor {...props} position={DEFAULT_ANCHOR_POSITION} />);
 
 describe("<Anchors />", () => {
+	afterEach(() => vi.clearAllMocks());
 	describe("Render", () => {
 		it("should render correctly with current path", () => {
 			useRouterMocked.mockReturnValue({ pathname: "any_href" });
@@ -52,6 +53,20 @@ describe("<Anchors />", () => {
 			expect(anchorEl).toHaveAttribute("aria-current", "true");
 			expect(anchorEl).toHaveAttribute("aria-disabled", "true");
 			expect(anchorEl).toHaveAttribute("tabIndex", "-1");
+			expect(screen.getByText(DEFAULT_ANCHOR.title)).toBeInTheDocument();
+			expect(screen.getByText(DEFAULT_ANCHOR_POSITION)).toBeInTheDocument();
+		});
+		it("should render correctly with different path", () => {
+			useRouterMocked.mockReturnValue({ pathname: "any_href_1" });
+			renderComponent();
+
+			const anchorEl = screen.getByTitle(`Ir para ${DEFAULT_ANCHOR.title}`);
+
+			expect(anchorEl).toBeInTheDocument();
+			expect(anchorEl).toHaveAttribute("href", DEFAULT_ANCHOR.href);
+			expect(anchorEl).toHaveAttribute("aria-current", "false");
+			expect(anchorEl).toHaveAttribute("aria-disabled", "false");
+			expect(anchorEl).toHaveAttribute("tabIndex", "0");
 			expect(screen.getByText(DEFAULT_ANCHOR.title)).toBeInTheDocument();
 			expect(screen.getByText(DEFAULT_ANCHOR_POSITION)).toBeInTheDocument();
 		});
