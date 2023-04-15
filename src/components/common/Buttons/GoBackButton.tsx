ee0c97e2ca97b70156134bdf0a1de6d6fdd00ec6
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-04-15 20:21:34 -0300

    feat: add styles to go-back-button component

--- src/components/common/Buttons/GoBackButton.tsx
+++ src/components/common/Buttons/GoBackButton.tsx
@@ -4,9 +4,14 @@ import { ArrowLeft } from "phosphor-react";
 export const GoBackButton = () => {
 	const { back } = useRouter();
 	return (
-		<button type="button" title="Voltar" onClick={back}>
+		<button
+			type="button"
+			title="Voltar"
+			onClick={back}
+			className="center--row gap-4 text-default font-medium text-heading transition-opacity hover:opacity-75 focus:opacity-75 dark:text-heading-dark"
+		>
 			<i>
-				<ArrowLeft />
+				<ArrowLeft size={20} />
 			</i>
 			Voltar
 		</button>
