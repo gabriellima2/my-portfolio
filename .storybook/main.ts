c1dc0a0af56eadedabfb9bf48f93731a347c5f4e
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-04-16 16:59:47 -0300

    chore: add module-alias config to storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -15,6 +16,15 @@ const config: StorybookConfig = {
   },
 	core: {
 		disableTelemetry: true
+	},
+	webpackFinal: async (config) => {
+		if (config.resolve) {
+			config.resolve.alias = {
+				...config.resolve?.alias,
+				"@": path.resolve(__dirname, "../src"),
+			};
+		}
+		return config;
 	}
 };
 export default config;
