893f2ed31e968666485fab057ac11e38e36a600d
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-04-16 19:42:31 -0300

    feat: add stories of the header component

---
+++ src/components/common/Headers/Header.stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { Header } from ".";
+
+type Story = StoryObj<typeof Header>;
+
+const meta: Meta<typeof Header> = {
+	title: "Common/Headers/Header",
+	component: Header,
+	tags: ["autodocs"],
+	args: {
+		anchors: [
+			{ href: "any_href", title: "Home" },
+			{ href: "any_href", title: "Docs" },
+			{ href: "any_href", title: "Blog" },
+		],
+	},
+};
+
+export const Default: Story = {
+	render: (props) => <Header {...props} />,
+};
+
+export default meta;
