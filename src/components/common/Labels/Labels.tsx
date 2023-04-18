9cea9daa20e69b68a447b245c57bf3be7e3640e9
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-04-18 19:22:48 -0300

    feat: add styles to labels component

--- src/components/common/Labels/Labels.tsx
+++ src/components/common/Labels/Labels.tsx
@@ -4,10 +4,14 @@ type LabelsProps = {
 
 export const Labels = (props: LabelsProps) => {
 	const { items } = props;
+	const itemsLength = items.length - 1;
 	return (
-		<ul>
-			{items.map((item) => (
-				<li key={item}>{item}</li>
+		<ul className="flex flex-wrap">
+			{items.map((item, i) => (
+				<li key={item} className="text-s">
+					{item}
+					{i < itemsLength && <span className="mx-1">|</span>}
+				</li>
 			))}
 		</ul>
 	);
