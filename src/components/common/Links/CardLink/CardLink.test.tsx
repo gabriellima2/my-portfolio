2f565c12838207d51c95f4b729430c3b1ae3c432
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-04-18 19:53:04 -0300

    feat: adds conditional arrow-icon to card-link component

--- src/components/common/Links/CardLink/CardLink.test.tsx
+++ src/components/common/Links/CardLink/CardLink.test.tsx
@@ -4,31 +4,36 @@ import { CardLink } from "./CardLink";
 const TAGS = ["any_label_1", "any_label_2", "any_label_3"];
 const TITLE = "any_title";
 const DESCRIPTION = "any_description";
+const ARROW_ICON = "arrow-icon";
 
 const renderComponent = (props: Parameters<typeof CardLink>[0]) =>
 	render(<CardLink {...props} />);
 
 describe("<CardLink />", () => {
 	describe("Render", () => {
-		it("should render correctly with all text props filled in", () => {
+		it("should render correctly", () => {
 			renderComponent({
 				tags: TAGS,
 				title: TITLE,
 				description: DESCRIPTION,
+				hasArrowIcon: true,
 			});
 
 			expect(screen.getByText(TITLE)).toBeInTheDocument();
 			expect(screen.getByText(DESCRIPTION)).toBeInTheDocument();
+			expect(screen.getByTestId(ARROW_ICON)).toBeInTheDocument();
 			TAGS.forEach((tag) => expect(screen.getByText(tag)).toBeInTheDocument());
 		});
 		it("should render correctly without props tags", () => {
 			renderComponent({
 				title: TITLE,
 				description: DESCRIPTION,
+				hasArrowIcon: true,
 			});
 
 			expect(screen.getByText(TITLE)).toBeInTheDocument();
 			expect(screen.getByText(DESCRIPTION)).toBeInTheDocument();
+			expect(screen.getByTestId(ARROW_ICON)).toBeInTheDocument();
 			TAGS.forEach((tag) =>
 				expect(screen.queryByText(tag)).not.toBeInTheDocument()
 			);
@@ -37,22 +42,39 @@ describe("<CardLink />", () => {
 			renderComponent({
 				tags: TAGS,
 				title: TITLE,
+				hasArrowIcon: true,
 			});
 
 			expect(screen.getByText(TITLE)).toBeInTheDocument();
 			expect(screen.queryByText(DESCRIPTION)).not.toBeInTheDocument();
+			expect(screen.getByTestId(ARROW_ICON)).toBeInTheDocument();
 			TAGS.forEach((tag) => expect(screen.getByText(tag)).toBeInTheDocument());
 		});
 		it("should render correctly without description and tags props", () => {
 			renderComponent({
 				title: TITLE,
+				hasArrowIcon: true,
 			});
 
 			expect(screen.getByText(TITLE)).toBeInTheDocument();
 			expect(screen.queryByText(DESCRIPTION)).not.toBeInTheDocument();
+			expect(screen.getByTestId(ARROW_ICON)).toBeInTheDocument();
 			TAGS.forEach((tag) =>
 				expect(screen.queryByText(tag)).not.toBeInTheDocument()
 			);
+		});
+		it("should render correctly without arrow icon", () => {
+			renderComponent({
+				tags: TAGS,
+				title: TITLE,
+				description: DESCRIPTION,
+				hasArrowIcon: false,
+			});
+
+			expect(screen.getByText(TITLE)).toBeInTheDocument();
+			expect(screen.getByText(DESCRIPTION)).toBeInTheDocument();
+			expect(screen.queryByTestId(ARROW_ICON)).not.toBeInTheDocument();
+			TAGS.forEach((tag) => expect(screen.getByText(tag)).toBeInTheDocument());
 		});
 	});
 });
--- src/components/common/Links/CardLink/CardLink.tsx
+++ src/components/common/Links/CardLink/CardLink.tsx
@@ -1,16 +1,25 @@
+import { ArrowUpRight } from "phosphor-react";
 import { Tags } from "../../Tags";
 
 type CardLinkProps = {
 	tags?: string[];
 	title: string;
 	description?: string;
+	hasArrowIcon?: boolean;
 };
 
 export const CardLink = (props: CardLinkProps) => {
-	const { title, description, tags } = props;
+	const { title, description, hasArrowIcon, tags } = props;
 	return (
 		<section>
-			<header>{tags && <Tags items={tags} />}</header>
+			<header>
+				{tags && <Tags items={tags} />}
+				{hasArrowIcon && (
+					<i data-testid="arrow-icon">
+						<ArrowUpRight />
+					</i>
+				)}
+			</header>
 			<h1>{title}</h1>
 			{description && <p>{description}</p>}
 		</section>
