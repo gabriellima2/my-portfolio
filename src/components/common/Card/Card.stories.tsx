a6b03adfedabf0e8246e44d06ee15feeab618c44
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-04-20 17:32:29 -0300

    feat: adds stories to card component

---
+++ src/components/common/Card/Card.stories.tsx
@@ -0,0 +1,29 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { Card } from ".";
+
+type Story = StoryObj<typeof Card>;
+
+const meta: Meta<typeof Card> = {
+	title: "Components/Common/Card",
+	component: Card,
+	tags: ["autodocs"],
+	decorators: [
+		(Story) => (
+			<div className="center--row">
+				<div className="w-[70%]">{Story()}</div>
+			</div>
+		),
+	],
+	args: {
+		tags: ["Lorem", "Ipsum"],
+		title: "Lorem ipsum dolor sit amet",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		hasArrowIcon: true,
+	},
+};
+
+export const Default: Story = {};
+
+export default meta;
