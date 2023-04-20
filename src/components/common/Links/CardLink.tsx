c4555d939a5340016285e57ba6e01852cf379e38
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-04-20 17:05:18 -0300

    feat: add new card-link component

---
+++ src/components/common/Links/CardLink.tsx
@@ -0,0 +1,22 @@
+import Link, { LinkProps } from "next/link";
+import { Card } from "../Card";
+
+type CardLinkProps = LinkProps &
+	Omit<Parameters<typeof Card>[0], "hasArrowIcon">;
+
+export const CardLink = (props: CardLinkProps) => {
+	const { title, description, tags, ...rest } = props;
+	return (
+		<Link
+			{...rest}
+			className="group transition-all hover:brightness-110 focus:brightness-110"
+		>
+			<Card
+				title={title}
+				description={description}
+				tags={tags}
+				hasArrowIcon={true}
+			/>
+		</Link>
+	);
+};
