d9928d4619cfff21a5efe28746c8c764e4a0d4f4
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-04-20 17:07:10 -0300

    fix: smooths transition from themes to icon component

--- src/components/common/Icon/Icon.tsx
+++ src/components/common/Icon/Icon.tsx
@@ -9,7 +9,7 @@ export const Icon = (props: IconProps) => {
 	return (
 		<i
 			{...rest}
-			className={`center--row rounded-full w-[36px] h-[36px] text-heading bg-util-primary border-2 border-util-secondary dark:border-util-secondary-dark dark:text-heading-dark dark:bg-util-primary-dark group-hover:border-transparent group-focus:border-transparent ${className}`}
+			className={`center--row rounded-full w-[36px] h-[36px] text-heading transition-all bg-util-primary border-2 border-util-secondary dark:border-util-secondary-dark dark:text-heading-dark dark:bg-util-primary-dark group-hover:border-transparent group-focus:border-transparent ${className}`}
 		>
 			{component()}
 		</i>
