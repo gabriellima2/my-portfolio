12f6be71ba212a5473ad9ba17bfd6e5173a5841d
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-04-25 17:33:29 -0300

    feat: adds link title props to card-link component

--- src/presentation/components/Links/CardLink.tsx
+++ src/presentation/components/Links/CardLink.tsx
@@ -2,13 +2,16 @@ import Link, { LinkProps } from "next/link";
 import { Card } from "../common/Card";
 
 type CardLinkProps = LinkProps &
-	Omit<Parameters<typeof Card>[0], "hasArrowIcon">;
+	Omit<Parameters<typeof Card>[0], "hasArrowIcon"> & {
+		linkTitle?: string;
+	};
 
 export const CardLink = (props: CardLinkProps) => {
-	const { title, description, tags, ...rest } = props;
+	const { linkTitle, title, description, tags, ...rest } = props;
 	return (
 		<Link
 			{...rest}
+			title={linkTitle}
 			className="group transition-all hover:brightness-110 focus:brightness-110"
 		>
 			<Card
