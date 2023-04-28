6d9381c0107da8e0cc348e70ccd4c72c6712fa79
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-04-28 18:04:46 -0300

    feat: add about page structure

---
+++ src/pages/sobre.tsx
@@ -0,0 +1,27 @@
+import {
+	Article,
+	ArticlePreview,
+	TextGroup,
+	Typography,
+} from "@/presentation/components";
+import { DefaultLayout } from "@/presentation/layouts/default-layout";
+
+export default function About() {
+	return (
+		<DefaultLayout>
+			<Article className="flex flex-col justify-between gap-21 lg:flex-row">
+				<div className="order-0 lg:order-1 w-[456px] h-[456px] rounded-2xl bg-util-primary  dark:bg-util-primary-dark" />
+				<TextGroup
+					title="Gabriel Lima"
+					paragraphs={[
+						"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+						"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+					]}
+				/>
+			</Article>
+			<ArticlePreview title="Contatos">
+				<Typography.Paragraph>Aqui</Typography.Paragraph>
+			</ArticlePreview>
+		</DefaultLayout>
+	);
+}
--- src/presentation/components/common/TextGroup/TextGroup.tsx
+++ src/presentation/components/common/TextGroup/TextGroup.tsx
@@ -8,7 +8,7 @@ type GroupTextProps = {
 export const TextGroup = (props: GroupTextProps) => {
 	const { title, paragraphs } = props;
 	return (
-		<section className="flex flex-col gap-6 md:gap-10 max-w-[590px]">
+		<section className="flex flex-col gap-6 md:gap-10 w-full max-w-[590px]">
 			<Typography.Display>{title}</Typography.Display>
 			{paragraphs.map((paragraph) => (
 				<Typography.Paragraph key={paragraph}>{paragraph}</Typography.Paragraph>
