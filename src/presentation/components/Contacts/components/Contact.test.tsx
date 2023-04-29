718b02c36866f0cd0db093588f7da8b2d90fea08
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-04-29 18:16:12 -0300

    feat: adds variants to contact component

--- src/presentation/components/Contacts/components/Contact.test.tsx
+++ src/presentation/components/Contacts/components/Contact.test.tsx
@@ -8,14 +8,14 @@ const renderComponent = (props: ContactProps) => render(<Contact {...props} />);
 
 describe("<Contact />", () => {
 	describe("Render", () => {
-		it("should render correctly with props filled", () => {
+		it("should render correctly with props filled and with-mask variant", () => {
 			const contact: IContact = {
 				title: "any_title",
 				href: "any_href",
 				mask: DEFAULT_MASK,
 			};
 
-			renderComponent({ ...contact });
+			renderComponent({ ...contact, variants: "with-mask" });
 
 			const linkEl = screen.getByText(contact.title);
 
@@ -23,13 +23,42 @@ describe("<Contact />", () => {
 			expect(screen.getByText(contact.mask!)).toBeInTheDocument();
 			expect(linkEl).toHaveAttribute("href", contact.href);
 		});
-		it("should render correctly with empty props mask", () => {
+		it("should render correctly with empty props mask and with-mask variant", () => {
 			const contact: IContact = {
 				title: "any_title",
 				href: "any_href",
 			};
 
-			renderComponent({ ...contact });
+			renderComponent({ ...contact, variants: "with-mask" });
+
+			const linkEl = screen.getByText(contact.title);
+
+			expect(linkEl).toBeInTheDocument();
+			expect(screen.queryByText(DEFAULT_MASK)).not.toBeInTheDocument();
+			expect(linkEl).toHaveAttribute("href", contact.href);
+		});
+		it("should render correctly with empty props mask and default variant", () => {
+			const contact: IContact = {
+				title: "any_title",
+				href: "any_href",
+			};
+
+			renderComponent({ ...contact, variants: "default" });
+
+			const linkEl = screen.getByText(contact.title);
+
+			expect(linkEl).toBeInTheDocument();
+			expect(screen.queryByText(DEFAULT_MASK)).not.toBeInTheDocument();
+			expect(linkEl).toHaveAttribute("href", contact.href);
+		});
+		it("should render correctly with props filled and default variant", () => {
+			const contact: IContact = {
+				title: "any_title",
+				href: "any_href",
+				mask: DEFAULT_MASK,
+			};
+
+			renderComponent({ ...contact, variants: "default" });
 
 			const linkEl = screen.getByText(contact.title);
 
--- src/presentation/components/Contacts/components/Contact.tsx
+++ src/presentation/components/Contacts/components/Contact.tsx
@@ -1,14 +1,17 @@
 import { IContact } from "@/shared/interfaces/IContact";
 
-export type ContactProps = IContact;
+export type ContactProps = IContact & {
+	variants?: "default" | "with-mask";
+};
 
 export const Contact = (props: ContactProps) => {
-	const { title, href, mask } = props;
+	const { title, href, mask, variants = "default" } = props;
+	const isToRenderTheMask = variants === "with-mask" && mask;
 	return (
 		<li>
 			<a href={href} title={`Entrar em contato via ${title}`}>
 				{title}
-				{mask && (
+				{isToRenderTheMask && (
 					<>
 						<span>:</span>
 						<span>{mask}</span>
