b5041281e11639ac7d89fdb8641111b5020f955c
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-05-01 17:16:09 -0300

    style: improves the organization of get-projects-controller tests

--- src/core/infrastructure/controllers/project-controllers/get-projects-controller.test.ts
+++ src/core/infrastructure/controllers/project-controllers/get-projects-controller.test.ts
@@ -18,70 +18,73 @@ const makeGetProjectsController = () =>
 
 describe("GetProjectsController", () => {
 	beforeEach(() => vi.clearAllMocks());
-	it("should throw an error if the limit number of projects is less than 0", async () => {
-		try {
-			const sut = makeGetProjectsController();
-			await sut.execute(-1);
-		} catch (err) {
-			expect(err).toThrow(Error);
-			const error = err as InvalidDataLimitError;
-			expect(error.message).toBe("Erro! O limite para o dado é inválido");
-			expect(error.statusCode).toBe(HttpStatusCode.badRequest);
-		}
-	});
-
-	it("should response correctly if not passed a limit number", async () => {
-		const sut = makeGetProjectsController();
-		executeMocked.mockReturnValueOnce({
-			body: [
-				...projectsMock,
-				{
-					href: "any_href",
-					id: "any_id",
-					techs: ["any_tech"],
-					title: "any_title",
-					description: "any_description",
-				},
-			],
-			ok: true,
+	describe("Errors", () => {
+		it("should throw an InvalidDataLimitError if the limit number of projects is less than 0", async () => {
+			try {
+				const sut = makeGetProjectsController();
+				await sut.execute(-1);
+			} catch (err) {
+				expect(err).toThrow(Error);
+				const error = err as InvalidDataLimitError;
+				expect(error.message).toBe("Erro! O limite para o dado é inválido");
+				expect(error.statusCode).toBe(HttpStatusCode.badRequest);
+			}
 		});
-		const response = await sut.execute();
-		if (typeof response.body === "string") return;
-
-		expect(response.body.length).toBeGreaterThan(PROJECTS_LIMIT);
-		expect(response.statusCode).toBe(HttpStatusCode.ok);
-		expect(response.ok).toBeTruthy();
+		it("should throw an UnexpectedError if ok is false", async () => {
+			try {
+				const sut = makeGetProjectsController();
+				executeMocked.mockReturnValueOnce({
+					body: null,
+					ok: false,
+				});
+				await sut.execute();
+			} catch (err) {
+				expect(err).toThrow(Error);
+				const error = err as UnexpectedError;
+				expect(error.message).toBe(
+					"Ocorreu um erro enquanto buscavamos pelos dados. Tente novamente"
+				);
+				expect(error.statusCode).toBe(HttpStatusCode.badRequest);
+			}
+		});
+		it("should throw an EmptyDataError if body is empty", async () => {
+			try {
+				const sut = makeGetProjectsController();
+				executeMocked.mockReturnValueOnce({
+					body: null,
+					ok: true,
+				});
+				await sut.execute();
+			} catch (err) {
+				expect(err).toThrow(Error);
+				const error = err as EmptyDataError;
+				expect(error.message).toBe("Desculpe, não encontramos nenhum dado!");
+				expect(error.statusCode).toBe(HttpStatusCode.notFound);
+			}
+		});
 	});
-	it("should throw an unexpected error if ok is false", async () => {
-		try {
+	describe("Success", () => {
+		it("should response correctly if not passed a limit number", async () => {
 			const sut = makeGetProjectsController();
 			executeMocked.mockReturnValueOnce({
-				body: null,
-				ok: false,
-			});
-			await sut.execute();
-		} catch (err) {
-			expect(err).toThrow(Error);
-			const error = err as UnexpectedError;
-			expect(error.message).toBe(
-				"Ocorreu um erro enquanto buscavamos pelos dados. Tente novamente"
-			);
-			expect(error.statusCode).toBe(HttpStatusCode.badRequest);
-		}
-	});
-	it("should throw an unexpected error if ok is false", async () => {
-		try {
-			const sut = makeGetProjectsController();
-			executeMocked.mockReturnValueOnce({
-				body: null,
+				body: [
+					...projectsMock,
+					{
+						href: "any_href",
+						id: "any_id",
+						techs: ["any_tech"],
+						title: "any_title",
+						description: "any_description",
+					},
+				],
 				ok: true,
 			});
-			await sut.execute();
-		} catch (err) {
-			expect(err).toThrow(Error);
-			const error = err as EmptyDataError;
-			expect(error.message).toBe("Desculpe, não encontramos nenhum dado!");
-			expect(error.statusCode).toBe(HttpStatusCode.notFound);
-		}
+			const response = await sut.execute();
+			if (typeof response.body === "string") return;
+
+			expect(response.body.length).toBeGreaterThan(PROJECTS_LIMIT);
+			expect(response.statusCode).toBe(HttpStatusCode.ok);
+			expect(response.ok).toBeTruthy();
+		});
 	});
 });
