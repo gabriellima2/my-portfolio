4efb926ff7b8a4be12c12dd02935aca4715b56b9
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-05-01 17:24:12 -0300

    style: improves the organization of get-projects-controller tests

--- src/core/infrastructure/controllers/project-controllers/get-projects-controller.test.ts
+++ src/core/infrastructure/controllers/project-controllers/get-projects-controller.test.ts
@@ -18,51 +18,7 @@ const makeGetProjectsController = () =>
 
 describe("GetProjectsController", () => {
 	beforeEach(() => vi.clearAllMocks());
-	describe("Errors", () => {
-		it("should throw an InvalidDataLimitError if the limit number of projects is less than 0", async () => {
-			try {
-				const sut = makeGetProjectsController();
-				await sut.execute(-1);
-			} catch (err) {
-				expect(err).toThrow(Error);
-				const error = err as InvalidDataLimitError;
-				expect(error.message).toBe("Erro! O limite para o dado é inválido");
-				expect(error.statusCode).toBe(HttpStatusCode.badRequest);
-			}
-		});
-		it("should throw an UnexpectedError if ok is false", async () => {
-			try {
-				const sut = makeGetProjectsController();
-				executeMocked.mockReturnValueOnce({
-					body: null,
-					ok: false,
-				});
-				await sut.execute();
-			} catch (err) {
-				expect(err).toThrow(Error);
-				const error = err as UnexpectedError;
-				expect(error.message).toBe(
-					"Ocorreu um erro enquanto buscavamos pelos dados. Tente novamente"
-				);
-				expect(error.statusCode).toBe(HttpStatusCode.badRequest);
-			}
-		});
-		it("should throw an EmptyDataError if body is empty", async () => {
-			try {
-				const sut = makeGetProjectsController();
-				executeMocked.mockReturnValueOnce({
-					body: null,
-					ok: true,
-				});
-				await sut.execute();
-			} catch (err) {
-				expect(err).toThrow(Error);
-				const error = err as EmptyDataError;
-				expect(error.message).toBe("Desculpe, não encontramos nenhum dado!");
-				expect(error.statusCode).toBe(HttpStatusCode.notFound);
-			}
-		});
-	});
+
 	describe("Success", () => {
 		it("should response correctly without limit number", async () => {
 			const sut = makeGetProjectsController();
@@ -87,7 +43,7 @@ describe("GetProjectsController", () => {
 			expect(response.ok).toBeTruthy();
 		});
 	});
-	it("should response correctly with limit number valid", async () => {
+	it("should response correctly with valid limit projects", async () => {
 		const sut = makeGetProjectsController();
 		executeMocked.mockReturnValueOnce({
 			body: projectsMock,
@@ -99,5 +55,50 @@ describe("GetProjectsController", () => {
 		expect(response.body.length).toBeGreaterThan(PROJECTS_LIMIT);
 		expect(response.statusCode).toBe(HttpStatusCode.ok);
 		expect(response.ok).toBeTruthy();
+	});
+	describe("Errors", () => {
+		it("should throw an InvalidDataLimitError with limit projects being less than 0", async () => {
+			try {
+				const sut = makeGetProjectsController();
+				await sut.execute(-1);
+			} catch (err) {
+				expect(err).toThrow(Error);
+				const error = err as InvalidDataLimitError;
+				expect(error.message).toBe("Erro! O limite para o dado é inválido");
+				expect(error.statusCode).toBe(HttpStatusCode.badRequest);
+			}
+		});
+		it("should throw an UnexpectedError with ok being false", async () => {
+			try {
+				const sut = makeGetProjectsController();
+				executeMocked.mockReturnValueOnce({
+					body: null,
+					ok: false,
+				});
+				await sut.execute();
+			} catch (err) {
+				expect(err).toThrow(Error);
+				const error = err as UnexpectedError;
+				expect(error.message).toBe(
+					"Ocorreu um erro enquanto buscavamos pelos dados. Tente novamente"
+				);
+				expect(error.statusCode).toBe(HttpStatusCode.badRequest);
+			}
+		});
+		it("should throw an EmptyDataError with body being empty", async () => {
+			try {
+				const sut = makeGetProjectsController();
+				executeMocked.mockReturnValueOnce({
+					body: null,
+					ok: true,
+				});
+				await sut.execute();
+			} catch (err) {
+				expect(err).toThrow(Error);
+				const error = err as EmptyDataError;
+				expect(error.message).toBe("Desculpe, não encontramos nenhum dado!");
+				expect(error.statusCode).toBe(HttpStatusCode.notFound);
+			}
+		});
 	});
 });
