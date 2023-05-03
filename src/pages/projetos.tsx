fc0c26304b7d4149335025569497b63173af5647
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-05-03 16:51:10 -0300

    chore: adds meta tags to projects page

--- src/pages/projetos.tsx
+++ src/pages/projetos.tsx
@@ -10,6 +10,7 @@ import {
 	Article,
 	TextGroup,
 	HandleError,
+	Head,
 } from "@/presentation/components";
 import { DefaultLayout } from "@/presentation/layouts/default-layout";
 
@@ -27,23 +28,30 @@ export default function Projects(props: ProjectsProps) {
 	);
 
 	return (
-		<DefaultLayout>
-			<Article>
-				<TextGroup
-					title="Projetos"
-					paragraphs={[
-						"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-					]}
-				/>
-			</Article>
-			<section className="border-b-2 border-b-util-secondary py-21 dark:border-b-util-secondary-dark">
-				<HandleError error={projects.error || errorForEmptyProjects}>
-					<ul className="grid grid-cols-1 gap-6 lg:grid-cols-2 xl:grid-cols-3">
-						<ProjectList projects={projects.data!} />
-					</ul>
-				</HandleError>
-			</section>
-		</DefaultLayout>
+		<>
+			<Head
+				description="Possuo diversos projetos desde pessoais até freelances. Sites para negócios, sites educacionais e aplicativos. Veja e teste essas aplicações!"
+				keywords="Sites, Aplicativos, Typescript, ReactJS, NextJS, VueJS, React Native"
+				path="Projetos"
+			/>
+			<DefaultLayout>
+				<Article>
+					<TextGroup
+						title="Projetos"
+						paragraphs={[
+							"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+						]}
+					/>
+				</Article>
+				<section className="border-b-2 border-b-util-secondary py-21 dark:border-b-util-secondary-dark">
+					<HandleError error={projects.error || errorForEmptyProjects}>
+						<ul className="grid grid-cols-1 gap-6 lg:grid-cols-2 xl:grid-cols-3">
+							<ProjectList projects={projects.data!} />
+						</ul>
+					</HandleError>
+				</section>
+			</DefaultLayout>
+		</>
 	);
 }
 
