31d2ce955b051977d1ba022182f9aa425d08c3ff
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-05-03 17:06:10 -0300

    chore: adds meta tags to blog page

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,12 +1,15 @@
-import { ComingSoon } from "@/presentation/components";
+import { ComingSoon, Head } from "@/presentation/components";
 import { DefaultLayout } from "@/presentation/layouts/default-layout";
 
 export default function Blog() {
 	return (
-		<DefaultLayout withoutFooter>
-			<section className="center--row pt-21">
-				<ComingSoon />
-			</section>
-		</DefaultLayout>
+		<>
+			<Head path="Blog" keywords="Coming soon" />
+			<DefaultLayout withoutFooter>
+				<section className="center--row pt-21">
+					<ComingSoon />
+				</section>
+			</DefaultLayout>
+		</>
 	);
 }
