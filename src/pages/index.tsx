b3061f9a494898cbc0491466576f8f19f20b0003
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-05-03 16:50:57 -0300

    chore: adds meta tags to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ import {
 	ComingSoon,
 	GradientBackground,
 	HandleError,
+	Head,
 	Projects,
 	TextGroup,
 } from "@/presentation/components";
@@ -31,38 +32,44 @@ export default function Home(props: HomeProps) {
 	);
 
 	return (
-		<GradientBackground>
-			<DefaultLayout>
-				<Article>
-					<div className="max-w-[587px]">
-						<TextGroup
-							title="Gabriel Lima"
-							paragraphs={[
-								"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-							]}
-						/>
-						<ArrowRightLink href="/sobre" className="mt-21">
-							Saber Mais
-						</ArrowRightLink>
-					</div>
-				</Article>
-				<ArticlePreview title="Projetos">
-					<HandleError error={projects.error || errorForEmptyProjects}>
-						<section>
-							<ul className="grid grid-rows-3 gap-6">
-								<Projects projects={projects.data!} />
-							</ul>
-							<ArrowRightLink href="/projetos" className="mt-16">
-								Ver Todos
+		<>
+			<Head
+				description="Me chamo Gabriel e sou um desenvolvedor front-end. Desenvolvo aplicações para plataformas Web e Mobile. Venha saber mais..."
+				keywords="Desenvolvedor, Frontend, Typescript, ReactJS, React Native, Mobile, Web"
+			/>
+			<GradientBackground>
+				<DefaultLayout>
+					<Article>
+						<div className="max-w-[587px]">
+							<TextGroup
+								title="Gabriel Lima"
+								paragraphs={[
+									"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+								]}
+							/>
+							<ArrowRightLink href="/sobre" className="mt-21">
+								Saber Mais
 							</ArrowRightLink>
-						</section>
-					</HandleError>
-				</ArticlePreview>
-				<ArticlePreview title="Blog">
-					<ComingSoon />
-				</ArticlePreview>
-			</DefaultLayout>
-		</GradientBackground>
+						</div>
+					</Article>
+					<ArticlePreview title="Projetos">
+						<HandleError error={projects.error || errorForEmptyProjects}>
+							<section>
+								<ul className="grid grid-rows-3 gap-6">
+									<Projects projects={projects.data!} />
+								</ul>
+								<ArrowRightLink href="/projetos" className="mt-16">
+									Ver Todos
+								</ArrowRightLink>
+							</section>
+						</HandleError>
+					</ArticlePreview>
+					<ArticlePreview title="Blog">
+						<ComingSoon />
+					</ArticlePreview>
+				</DefaultLayout>
+			</GradientBackground>
+		</>
 	);
 }
 
