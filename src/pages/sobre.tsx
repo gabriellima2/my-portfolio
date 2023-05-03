8cbff7bf3a6cab5236abd7b74681ae5186d6b00e
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-05-03 17:06:18 -0300

    chore: adds meta tags to about page

--- src/pages/sobre.tsx
+++ src/pages/sobre.tsx
@@ -2,6 +2,7 @@ import {
 	Article,
 	ArticlePreview,
 	Avatar,
+	Head,
 	TextGroup,
 } from "@/presentation/components";
 import { Contacts } from "@/presentation/components/Contacts/Contacts";
@@ -10,24 +11,27 @@ import { contacts } from "@/shared/assets";
 
 export default function About() {
 	return (
-		<DefaultLayout>
-			<Article className="flex flex-col justify-between gap-21 lg:flex-row lg:items-center">
-				<Avatar
-					src="/assets/photo.jpg"
-					alt="Foto de Gabriel Lima"
-					className="lg:order-1"
-				/>
-				<TextGroup
-					title="Gabriel Lima"
-					paragraphs={[
-						"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-						"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-					]}
-				/>
-			</Article>
-			<ArticlePreview title="Contatos">
-				<Contacts contacts={contacts} variants="with-mask" />
-			</ArticlePreview>
-		</DefaultLayout>
+		<>
+			<Head path="Sobre" keywords="" />
+			<DefaultLayout>
+				<Article className="flex flex-col justify-between gap-21 lg:flex-row lg:items-center">
+					<Avatar
+						src="/assets/photo.jpg"
+						alt="Foto de Gabriel Lima"
+						className="lg:order-1"
+					/>
+					<TextGroup
+						title="Gabriel Lima"
+						paragraphs={[
+							"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+							"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+						]}
+					/>
+				</Article>
+				<ArticlePreview title="Contatos">
+					<Contacts contacts={contacts} variants="with-mask" />
+				</ArticlePreview>
+			</DefaultLayout>
+		</>
 	);
 }
