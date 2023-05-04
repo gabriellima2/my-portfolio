3effc2c5c4d48a6567a8bda90b3ea56b2b0b6917
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-05-04 16:54:12 -0300

    fix: change page title implementation

--- src/presentation/components/Head.tsx
+++ src/presentation/components/Head.tsx
@@ -2,8 +2,7 @@ import NextHead from "next/head";
 
 type HeadProps = {
 	description?: string;
-	title?: string;
-	path?: string;
+	path: string;
 	keywords: string;
 };
 
@@ -12,16 +11,13 @@ export const Head = (props: HeadProps) => {
 		keywords,
 		description = "Me chamo Gabriel e sou um desenvolvedor front-end. Desenvolvo aplicações para plataformas Web e Mobile. Possuo diversos projetos desde pessoais até freelances. Sendo, sites para negócios, sites educacionais e aplicativos. Venha saber mais...",
 		path = "Portfólio",
-		title = "Gabriel Lima",
 	} = props;
 	return (
 		<NextHead>
 			<meta name="author" content="Gabriel Lima" />
 			<meta name="description" content={description} />
 			<meta name="keywords" content={keywords} />
-			<title>
-				{title} {path && `- ${path}`}
-			</title>
+			<title>{`Gabriel Lima - ${path}`}</title>
 		</NextHead>
 	);
 };
