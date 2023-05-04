c35bd02817d264e99998e8ba4a550d73cca777b2
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-05-04 16:16:17 -0300

    feat: add set method to cookies-adapter implementation using nookies

--- src/core/infrastructure/adapters/cookies-adapter.ts
+++ src/core/infrastructure/adapters/cookies-adapter.ts
@@ -3,13 +3,21 @@ import { ICookiesAdapter } from "@/core/domain/adapters";
 
 export class CookiesAdapter implements ICookiesAdapter {
 	get<T>(key: string): T | null {
-		const { [key]: valueJSON } = parseCookies();
-		if (!valueJSON) return null;
-		const value: T = JSON.parse(valueJSON);
+		const { [key]: valueInJSON } = parseCookies();
+		if (!valueInJSON) return null;
+		const value: T = JSON.parse(valueInJSON);
 		return value;
 	}
 
-	set<T>(key: string, value: T, maxAge: number): T | null {}
+	set<T>(key: string, value: T, maxAge: number): void {
+		const valueInJSON = JSON.stringify(value);
+		setCookie(null, key, valueInJSON, {
+			maxAge: maxAge,
+			sameSite: "strict",
+			path: "/",
+			secure: true,
+		});
+	}
 
 	remove<T>(key: string): T | null {}
 }
