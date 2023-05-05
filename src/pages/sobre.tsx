f9c4a7e3f7a2e45869409d19e9fd73919051d48e
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-05-05 17:09:01 -0300

    feat: add text content to about page

--- src/pages/sobre.tsx
+++ src/pages/sobre.tsx
@@ -12,7 +12,11 @@ import { contacts } from "@/shared/assets";
 export default function About() {
 	return (
 		<>
-			<Head path="Sobre" keywords="" />
+			<Head
+				path="Sobre"
+				keywords="Desenvolvedor, Front-end, Criativo, Experiência, Sites, Clientes, Problemas, Design, Codificação, Deploy, Mobile, React Native, ReactJS, Styled-Components, Typescript, Docker, Testes Unitários, Figma, Git, Redux"
+				description="Sou um desenvolvedor front-end criativo com experiência em criar sites modernos para clientes que desejam aumentar sua visibilidade online. Gosto de criar projetos pessoais resolvendo problemas que encontro diariamente. Tenho habilidades em design, codificação e deploy das aplicações..."
+			/>
 			<DefaultLayout>
 				<Article className="flex flex-col justify-between gap-21 lg:flex-row lg:items-center">
 					<Avatar
@@ -23,8 +27,8 @@ export default function About() {
 					<TextGroup
 						title="Gabriel Lima"
 						paragraphs={[
-							"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-							"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+							"Sou um desenvolvedor front-end criativo com experiência em criar sites modernos para clientes que desejam aumentar sua visibilidade online. Gosto de criar projetos pessoais resolvendo problemas que encontro diariamente, focando em facilitar a experiência e a vida das pessoas. Tenho habilidades em design, codificação e deploy das aplicações usando tecnologias que englobam o ReactJS e ferramentas de design e hospedagem. Também desenvolvo para plataforma mobile com React Native.",
+							"Minha stack se baseia em React com Javascript e Typescript, sendo tecnologias populares, como NextJS, React Native, Redux, Redux-Toolkit, StitchesJS, TailwindCSS e Styled-Components. Além disso, possuo conhecimentos em Testes Unitários, Git, Docker e Figma.",
 						]}
 					/>
 				</Article>
