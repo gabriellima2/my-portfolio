3ace9a624b9a0d3aef6aa9cf655396902d23867b
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-05-06 15:52:14 -0300

    chore: updates tailwind content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-		"./src/**/*.{js,ts,jsx,tsx}"
+		"./src/**/*.{js,ts,jsx,tsx}",
+		"./src/**/**/*.{js,ts,jsx,tsx}",
+		"./src/**/**/**/*.{js,ts,jsx,tsx}",
+		"./src/**/**/**/**/*.{js,ts,jsx,tsx}",
+		"./src/**/**/**/**/**/*.{js,ts,jsx,tsx}",
+		"./src/**/**/**/**/**/**/*.{js,ts,jsx,tsx}",
 	],
 	darkMode: "class",
   theme: {
