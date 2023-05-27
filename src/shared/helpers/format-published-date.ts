96983bdb721d9ea60522e5bb378d01443353fcc1
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-05-27 17:34:04 -0300

    feat: implements the function to format date

--- src/shared/helpers/format-published-date.ts
+++ src/shared/helpers/format-published-date.ts
@@ -1,3 +1,14 @@
-export function formatPublishedDate(date: string) {
-	return date;
+import { getMonthInTextFormatFromNumber } from "./get-month-in-text-format-from-number";
+
+/**
+ *
+ * @param value: Expected format: 2023-12-31
+ * @returns Value formatted as: 31 Dez 2023
+ */
+export function formatPublishedDate(value: string) {
+	if (!value.includes("-")) throw new Error("Invalid date format");
+	const lastSeparatorIndex = value.lastIndexOf("-");
+	const [year, month, day] = value.slice(0, lastSeparatorIndex + 3).split("-");
+	const abbrMonthText = getMonthInTextFormatFromNumber[month].slice(0, 3);
+	return `${day} ${abbrMonthText} ${year}`;
 }
