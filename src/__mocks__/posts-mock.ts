a3b3fd640a36c60ed3b4e3054687682cbbba6f50
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-05-29 18:39:18 -0300

    test: fix render test of posts component

--- src/__mocks__/posts-mock.ts
+++ src/__mocks__/posts-mock.ts
@@ -5,7 +5,7 @@ export const postsMock: PostEntity[] = [
 		title: "Aplicando Testes Unitários no ReactJS com Vitest",
 		tags: ["any_tag"],
 		slug: "aplicando-testes-unitarios-no-reactjs-com-vitest",
-		publishedAt: "17 Jun 2023",
+		publishedAt: "2023-06-17",
 		content: { html: "any_html" },
 	},
 ];
