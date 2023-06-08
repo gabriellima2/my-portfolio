13789a867fc215780d27ae74849f064a400b50f1
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-06-08 19:23:58 -0300

    fix: word wrap in tags component

--- src/presentation/components/common/Tags/Tags.tsx
+++ src/presentation/components/common/Tags/Tags.tsx
@@ -7,9 +7,9 @@ export const Tags = (props: TagsProps) => {
 	const { items, className } = props;
 	const itemsLength = items.length - 1;
 	return (
-		<ul className={`flex flex-1 ${className}`}>
+		<ul className={`flex flex-1 flex-wrap ${className}`}>
 			{items.map((item, i) => (
-				<li key={item} className="text-s">
+				<li key={item} className="truncate text-s">
 					{item}
 					{i < itemsLength && <span className="mx-1">|</span>}
 				</li>
